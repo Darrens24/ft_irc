0bf12d00c9f1a91e6a722d8e9fd346fff1ca1f25
Xq333 <xq1.3x3@gmail.com>
2023-10-31 15:50:08 +0100

    :sparkles: BOT WOR KING

--- headers/Bot.hpp
+++ headers/Bot.hpp
@@ -1,16 +1,40 @@
 #pragma once
 
-#include "../headers/Server.hpp"
+#define RED "\033[1;31m" // FOR DESTRUCTORS
+#define GRN "\033[1;32m" // FOR CONSTRUCTORS
+#define YEL "\033[1;33m" // FOR ERRORS
+#define BLU "\033[1;34m"
+#define MAG "\033[1;35m"
+#define CYA "\033[1;36m"
+#define W "\033[1;97m"
+#define WHT "\033[1;97m"
+#define NC "\033[1;0m"
+#define I "\033[1;3m"
+#define U "\033[1;4m"
+
+#include <arpa/inet.h>
+#include <cstring>
+#include <fcntl.h>
+#include <iostream>
+#include <map>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <poll.h>
+#include <stdlib.h>
+#include <string>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <vector>
 
 class Bot {
 public:
-  Bot(Server *server);
+  Bot(int port, std::string password);
   ~Bot();
   Bot(const Bot &src);
   Bot &operator=(const Bot &rhs);
 
   /***\ GETTERS \***/
-  Server *getServer() const { return _server; }
   int getBotSocket() const { return _botSocket; }
   int getBotPort() const { return _botPort; }
   std::string getNickname() const { return _nickname; }
@@ -24,7 +48,6 @@ public:
   void response(std::string message);
 
 private:
-  Server *_server;
   int _botSocket;
   int _botPort;
   std::string _nickname;
--- srcs/bot/Bot.cpp
+++ srcs/bot/Bot.cpp
@@ -1,18 +1,16 @@
 #include "../../headers/Bot.hpp"
-#include <sys/socket.h>
 
-int main(int ac, char **av) { Bot return 0; }
+#define host "localhost";
 
 /***\ CONSTRUCTORS \***/
 
-Bot::Bot(Server *server) : _server(server) {
+Bot::Bot(int port, std::string password)
+    : _botPort(port), _botPassword(password) {
   this->_botMessages.push_back("Bienvenue a tout le monde sur le serveur !");
   this->_botMessages.push_back("Je suis un bot !");
   this->_nickname = "Sunbot";
   this->_username = "Sunbot";
   this->_realname = "elbot";
-  this->_botPassword = server->getPassword();
-  this->_botPort = server->getPort();
 }
 
 Bot::~Bot() {}
@@ -21,7 +19,6 @@ Bot::Bot(const Bot &src) { *this = src; }
 
 Bot &Bot::operator=(const Bot &rhs) {
   if (this != &rhs) {
-    this->_server = rhs._server;
     this->_nickname = rhs._nickname;
     this->_username = rhs._username;
     this->_realname = rhs._realname;
@@ -35,28 +32,78 @@ Bot &Bot::operator=(const Bot &rhs) {
 /***\ METHODS \***/
 
 bool Bot::connectToServer() {
+  struct hostent *hostinfo;
+  struct sockaddr_in server_addr;
+
+  const char *hostname = host;
+  if ((hostinfo = gethostbyname(hostname)) == NULL) {
+    return (false);
+  }
+
   this->_botSocket = socket(AF_INET, SOCK_STREAM, 0);
   if (this->_botSocket == -1) {
     std::cerr << "Error: socket creation failed" << std::endl;
     return false;
   }
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_port = htons(this->_botPort);
+  server_addr.sin_addr = *(struct in_addr *)(hostinfo->h_addr);
 
-  if (connect(this->_botSocket, (struct sockaddr *)&this->_server->getAddr(),
-              sizeof(this->_server->getAddr())) == -1) {
+  if (connect(this->_botSocket, (struct sockaddr *)(&server_addr),
+              sizeof(struct sockaddr)) == -1) {
     std::cerr << "Error: connection failed" << std::endl;
     return false;
   }
 
-  std::string password = "PASS " + this->_botPassword + "\n";
-  std::string nickname = "NICK " + this->_nickname + "\n";
+  std::string password = "PASS " + this->_botPassword + "\r\n";
+  std::string nickname = "NICK " + this->_nickname + "\r\n";
   std::string username = "USER " + this->_username + " 0 * " + this->_realname;
   std::string everything = password + nickname + username;
 
   this->response(everything);
+  while (1) {
+    char buffer[1024];
+    memset(buffer, 0, 1024);
+
+    ssize_t read = recv(this->_botSocket, buffer, 1024, 0);
+    if (read < 0) {
+      std::cout << RED "Error: Recv failed" NC << std::endl;
+      exit(EXIT_FAILURE);
+    }
+
+    std::string strbuffer = buffer;
+
+    std::cout << "J'AI RECU: " << strbuffer << std::endl;
+  }
   return true;
 }
 
 void Bot::response(std::string message) {
   std::string response = message + "\r\n";
   send(this->_botSocket, response.c_str(), response.length(), 0);
+}
+
+int myAtoi3(char *str) {
+  int res = 0;
+  int sign = 1;
+  int i = 0;
+
+  if (str[0] == '-') {
+    sign = -1;
+    i++;
+  }
+  for (; str[i] != '\0'; ++i)
+    res = res * 10 + str[i] - '0';
+  return sign * res;
+}
+
+int main(int ac, char **av) {
+  if (ac != 3) {
+    std::cout << YEL "Usage: ./bot <port> <password>" NC << std::endl;
+    return -1;
+  }
+  Bot bot(myAtoi3(av[1]), av[2]);
+
+  bot.connectToServer();
+  return 0;
 }
